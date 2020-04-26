5642c0c019f686e31a10a73d368b7c2e411b6154
jennifer klein <jenniejoyness@gmail.com>
2020-04-26 15:35:15 +0300

    nodeee

---
+++ Node.py
@@ -0,0 +1,13 @@
+import hashlib
+
+class Node:
+
+    def __init__(self, left, right, hash_value=None):
+        self.left = left
+        self.right = right
+        self.hash_value = self.calc_hash() if not hash_value else hash_value
+
+
+    def calc_hash(self):
+        return str(hashlib.sha256((self.left.hash_value + self.right.hash_value).encode()).hexdigest())
+
